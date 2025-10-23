6dae7a42039ecd4430bd1b9dd8f87b5bf27835ac
Frédéric FRANCE <frederic.france@free.fr>
2025-10-23 16:00:02 +0200

    add vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '90fe509e82fc701a9a3ee4f796bbd14e8eb5b014',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '90fe509e82fc701a9a3ee4f796bbd14e8eb5b014',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'fabianwennink/iconcaptcha' => array(
+            'pretty_version' => '4.0.6',
+            'version' => '4.0.6.0',
+            'reference' => 'a64c3799c4d43f56df0abe472e2e2d0522536eef',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../fabianwennink/iconcaptcha',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
